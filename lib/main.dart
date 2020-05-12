19ae2f991fc86289c98ff1d76eaef85c4a0996dc
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2020-05-12 19:50:58 +0530

    Add main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,39 @@
+// Include all the required files
+// Built in Libraries
 import 'package:flutter/material.dart';
+// Utils
+import './utils/light_theme.dart';
+import './utils/dark_theme.dart';
+// Custom components screens library
+import './components/home_page.dart';
 
+// Main function - Starting point of the program.
 void main() {
   runApp(MyApp());
 }
 
+// MyApp - Starting widget of the app
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Use the material app theming and design.
     return MaterialApp(
-      title: 'Ara',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Ara'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      title: 'Ara', // Main title of the application
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      // All the routes to the app screens
+      routes: {
+        '/':(context) => HomePage(title: 'Ara',),
+        '/settings':(context) => Scaffold(),
+        '/help':(context) => Scaffold(),
+        '/privacy':(context) => Scaffold(),
+        '/terms':(context) => Scaffold(),
+        '/about':(context) => Scaffold(),
+      },
     );
   }
 }
