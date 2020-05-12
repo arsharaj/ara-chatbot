2bdae69fdf7c44f0babc119f6b0600e1fe63d8b3
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2020-05-12 19:50:10 +0530

    Add the colors pallete

---
+++ lib/utils/colors.dart
@@ -0,0 +1,17 @@
+// Include all the required files
+import 'package:flutter/material.dart';
+
+// Light Theme colors
+const primaryColorLight = Color(0xFF4DB6AC);
+const accentColorLight = Color(0xFF338A3E);
+const backgroundColorLight = Color(0xFFD6FFFB);
+const popupColorLight = Color(0xFFBDF2ED);
+
+// Dark Theme colors
+const primaryColorDark = Color(0xFF004c46);
+const accentColorDark = Color(0xFF98ee99);
+const backgroundColorDark = Color(0xFF007169);
+const popupColorDark = Color(0xFF1E6C22);
+
+// Not a theme specific color
+const iconColor = Color(0xFFBDF2ED);
