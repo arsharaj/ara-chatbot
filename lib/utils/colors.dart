1e45d3b7d7baaa255a7e305d3d15d8297bc77d5a
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2020-05-14 15:22:39 +0530

    Minor updates

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,12 +6,16 @@ const primaryColorLight = Color(0xFF4DB6AC);
 const accentColorLight = Color(0xFF338A3E);
 const backgroundColorLight = Color(0xFFD6FFFB);
 const popupColorLight = Color(0xFFBDF2ED);
+const textColorLight = Color(0xFF000000);
+const backgroundColorShadeLight = Color(0x11000000);
 
 // Dark Theme colors
 const primaryColorDark = Color(0xFF004c46);
 const accentColorDark = Color(0xFF98ee99);
 const backgroundColorDark = Color(0xFF007169);
 const popupColorDark = Color(0xFF115325);
+const textColorDark = Color(0xFFFFFFFF);
+const backgroundColorShadeDark = Color(0x11FFFFFF);
 
 // Not a theme specific color
 const iconColor = Color(0xFFBDF2ED);
