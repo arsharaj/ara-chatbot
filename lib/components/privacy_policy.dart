d1e07529def8da883574abf66f1378f556bff002
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2020-05-15 14:19:44 +0530

    Add privacy policy

--- lib/components/privacy_policy.dart
+++ lib/components/privacy_policy.dart
@@ -1,7 +1,8 @@
 // Import all the required files
 import 'package:flutter/material.dart';
 import 'package:ara_chatbot/utils/colors.dart';
-import 'package:ara_chatbot/utils/privacy_card.dart';
+import 'package:ara_chatbot/utils/card_template.dart';
+import 'package:ara_chatbot/utils/privacy_data.dart';
 
 // Privacy policy of the app is rendered here.
 class PrivacyPolicy extends StatelessWidget {
@@ -22,9 +23,53 @@ class PrivacyPolicy extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          PrivacyCard(
-            heading: "Test",
-            body: "Test the policy card",
+          // Update Date and Abstract Intro
+          CardTemplate(
+            heading: updateDate,
+            body: abstractIntro,
+            status: 1,
+          ),
+          // Introduction
+          CardTemplate(
+            heading: introHeading,
+            body: introBody,
+            status: 0,
+          ),
+          // Log Data
+          CardTemplate(
+            heading: logHeading,
+            body: logBody,
+            status: 1,
+          ),
+          // Communications
+          CardTemplate(
+            heading: commHeading,
+            body: commBody,
+            status: 0,
+          ),
+          // Cookies
+          CardTemplate(
+            heading: cookieHeading,
+            body: cookieBody,
+            status: 1,
+          ),
+          // Security
+          CardTemplate(
+            heading: securityHeading,
+            body: securityBody,
+            status: 0,
+          ),
+          // Changes
+          CardTemplate(
+            heading: changesHeading,
+            body: changesBody,
+            status: 1,
+          ),
+          // Contact Me
+          CardTemplate(
+            heading: contactHeading,
+            body: contactBody,
+            status: 0,
           ),
         ],
       ),
---
+++ lib/utils/privacy_data.dart
@@ -0,0 +1,70 @@
+// Last Update
+const updateDate="Last updated: 11 May 2020";
+
+// Abstract
+const abstractIntro="""
+I, Arsharaj Chauhan operates Ara Chatbot app. This policy will inform you everything regarding the collection, use and 
+disclosure of Personal Information I receive from users of the app.
+
+I use your Personal Information only for providing and improving the app. By using this app, you agree to the collection 
+and use of information in accordance with this policy.""";
+
+// Introduction
+const introHeading="Information Collection And Use";
+const introBody="""
+While using my app, I may ask you to provide me with certain personally identifiable information that can be used to contact 
+or identify you. Personally identifiable information may include, but is not limited to your name, email address, search 
+history.
+
+This collected information is used to improve the system and provide better services.""";
+
+// Log Data
+const logHeading="Log Data";
+const logBody="""
+Like many apps, I may use third party services such as Google Analytics that collect, monitor and analyze this information 
+for better experience. I explicity mention that, I use Dialogflow as a third party agent to provide human-like interaction
+in the app, the data is logged here anonymously for improvement of the system.""";
+
+// Communications
+const commHeading="Communications";
+const commBody="""
+I may use your Personal Information to contact you with newsletters, marketing or promotional materials and other information 
+that include your social profile links that you provide me.""";
+
+// Cookies
+const cookieHeading="Cookies";
+const cookieBody="""
+Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your 
+browser from a web site and stored on your computer's hard drive.
+
+Ara does not use cookies in any form. As soon as you leave the app everything is wiped. So next time it will be a fresh 
+relaunch.""";
+
+// Security
+const securityHeading="Security";
+const securityBody="""
+The security of your Personal Information is important to me, but remember that no method of transmission over the Internet, 
+or method of electronic storage, is 100% secure. While I strive to use commercially acceptable means to protect your 
+Personal Information, I cannot guarantee its absolute security.""";
+
+// Changes
+const changesHeading="Changes To This Privacy Policy";
+const changesBody="""
+This Privacy Policy is effective as of 11 May 2020 and will remain in effect except with respect to any changes in its 
+provisions in the future, which will be in effect immediately after being posted on this page.
+
+I reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. 
+Your continued use of the Service after we post any modifications to the Privacy Policy on this page will constitute your 
+acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.
+
+If I make any material changes to this Privacy Policy, I will notify you either through the email address if provided or by
+sending notifications.""";
+
+// Contact Me
+const contactHeading="Contact Me";
+const contactBody="""
+If you have any questions about this Privacy Policy, please contact me via email or any other social account.
+
+Email Address : arsharajchauhan343@gmail.com
+Twitter : @arsharajchauhan
+Github : https://github.com/arsharaj""";
