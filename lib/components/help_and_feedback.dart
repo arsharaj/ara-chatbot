436e3829a6a37289d88bcddb6c14e86765173b75
Arsharaj <arsharajchauhan343@gmail.com>
2020-05-18 16:57:10 +0530

    Add the completed help and feddback ui

--- lib/components/help_and_feedback.dart
+++ lib/components/help_and_feedback.dart
@@ -7,14 +7,30 @@ import 'package:google_fonts/google_fonts.dart';
 class HelpAndFeedback extends StatelessWidget {
   // The title of the application
   final String title;
+  // Controller for the textfield for handling text
   final TextEditingController controller = TextEditingController();
 
   // Constructor
-  HelpAndFeedback({Key key, this.title}) : super(key: key);  
+  HelpAndFeedback({Key key, this.title}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {   
-    final Color backgroundColor = (MediaQuery.of(context).platformBrightness == Brightness.light) ? backgroundColorLight : backgroundColorDark;
+  Widget build(BuildContext context) {
+    final Color backgroundColor =
+        (MediaQuery.of(context).platformBrightness == Brightness.light)
+            ? backgroundColorLight
+            : backgroundColorDark;
+    final Color textColor =
+        (MediaQuery.of(context).platformBrightness == Brightness.light)
+            ? textColorLight
+            : textColorDark;
+    final Color buttonTextColor =
+        (MediaQuery.of(context).platformBrightness == Brightness.dark)
+            ? textColorLight
+            : textColorDark;
+    final Color buttonColor =
+        (MediaQuery.of(context).platformBrightness == Brightness.dark)
+            ? accentColorDark
+            : accentColorLight;
 
     return Scaffold(
       backgroundColor: backgroundColor,
@@ -27,9 +43,10 @@ class HelpAndFeedback extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              "Submit an issue",
+              "Descibe an issue",
               style: GoogleFonts.merriweather(
                 fontSize: 16,
+                color: textColor,
               ),
             ),
             SizedBox(
@@ -38,26 +55,87 @@ class HelpAndFeedback extends StatelessWidget {
             Row(
               children: <Widget>[
                 SizedBox(
-                  width: (MediaQuery.of(context).size.width)-90,
+                  width: (MediaQuery.of(context).size.width) - 90,
                   child: TextFormField(
-                  autocorrect: true,
-                  controller: controller,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    fillColor: accentColorDark,
-                    filled: true, 
-                  ), 
+                    autocorrect: true,
+                    controller: controller,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () => controller.clear(),
+                      ),
+                      border: InputBorder.none,
+                      fillColor: accentColorDark,
+                      filled: true,
+                    ),
+                  ),
                 ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  color: buttonColor,
+                  onPressed: () {
+                    // TODO
+                    if (controller.text == "") {
+                      print("Empty text field!!");
+                    } else {
+                      print(controller.text);
+                    }
+                    controller.text = "";
+                  },
                 ),
-              IconButton(
-              icon: Icon(Icons.send), 
-              color: backgroundColorDark,
-              onPressed: (){
-                print(controller.value.text);
-              },
-            ),
               ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "How much do you like Ara?",
+                    style: GoogleFonts.merriweather(
+                      color: textColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Rate on Google Playstore",
+                    ),
+                    color: buttonColor,
+                    textColor: buttonTextColor,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => Navigator.pushNamed(context, "/privacy"),
+                    child: Text(
+                      "Privacy Policy",
+                      style: GoogleFonts.merriweather(
+                        color: textColor,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.pushNamed(context, "/terms"),
+                    child: Text(
+                      "Terms of Services",
+                      style: GoogleFonts.merriweather(
+                        color: textColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
